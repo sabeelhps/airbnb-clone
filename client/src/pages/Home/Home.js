1a6dd116f7818208a3d361a9a21ca3801cd7369f
Saurabh kashyap <saurabhkashyap201@gmail.com>
2023-06-04 15:40:19 +0530

    added home css module filed and added a basic navbar for homepage

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,19 @@
 import React, { Fragment } from 'react';
+import styles from './Home.module.css';
 import { Heading, SimpleGrid, Box } from '@chakra-ui/react';
 import PropertyCard from '../../components/PropertyCard/PropertyCard';
 
 const Home = () => {
   return (
     <Fragment>
+      <nav >
+        <ul className={styles['navbar-home']}>
+          <li>AirBnb</li>
+          <li>Anywhere</li>
+          <li>Any week</li>
+          <li> Add Guest</li>
+        </ul>
+      </nav>
       <Heading>I'm a Heading</Heading>
       <SimpleGrid columns={{sm: 2, md: 3, lg:4}} spacing='10px'>
         {/* <Box bg='tomato'></Box> */}
